1c809cbf3e9dcbdbdb7aac08a18d58b594ea34a3
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-12-21 19:33:49 +0100

    map extention check done

--- parsing/main.c
+++ parsing/main.c
@@ -6,13 +6,38 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 01:20:11 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/12/19 01:21:26 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/12/21 19:33:10 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<libc.h>
 
+
+//.cub
+void	check_map_extension(char **av)
+{
+	int j;
+
+	j = 0;
+	while (av[1][j])
+		j++;
+	if (av[1][j - 1] == 'b' && av[1][j - 2] == 'u' && av[1][j - 3] == 'c' && av[1][j - 4] == '.')
+		return;
+	else
+	{
+		printf("ERROR: invalid map extention\n");
+		exit(1);
+	}
+}
+
+
+
 int main(int ac, char **av)
 {
-    printf("AYO from parsing and textures\n");
+	if (ac != 2)
+	{
+		printf("ERROR: invalid number of arguments\n");
+		exit(1);
+	}
+	check_map_extension(av);
 }
